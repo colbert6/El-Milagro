9381ef060428edca8a94b137afc4fedcf313e194
colbert6 <colbersiho_6696@hotmail.com>
2016-02-19 16:13:44 -0500

    oriflame de 5

--- views/producto/js/funciones_form.js
+++ views/producto/js/funciones_form.js
@@ -31,7 +31,7 @@ $(function() {
     });
     
     $( "#save" ).click(function(){
-         if( $("#codigo_barra").val()!='' && ($("#codigo_barra").val().length!=13 && $("#codigo_barra").val().length!=12 && $("#codigo_barra").val().length!=8 && $("#codigo_barra").val().length!=7)){
+         if( $("#codigo_barra").val()!='' && ($("#codigo_barra").val().length!=13 && $("#codigo_barra").val().length!=12 && $("#codigo_barra").val().length!=8 && $("#codigo_barra").val().length!=7&& $("#codigo_barra").val().length!=5)){
            $("#codigo_barra").css('border','solid 1px red');
             $("#codigo_barra").html('<label class="lbl_msg">Debes llenar todos los campos necesarios</label>');
             $("#codigo_barra").focus();    
@@ -89,7 +89,7 @@ $(function() {
     
     $("#codigo_barra").blur(function(){
         
-        if($(this).val()!='' && ($(this).val().length==13 || $(this).val().length==12 || $(this).val().length==8 || $(this).val().length==7)){
+        if($(this).val()!='' && ($(this).val().length==13 || $(this).val().length==12 || $(this).val().length==8 || $(this).val().length==7 || $(this).val().length==5)){
             $.post(url+'producto/buscador','codigo_barra='+$(this).val(),function(datos){
                 if(datos.length>0 ){
                     if($("#id_producto").val()==datos[0].id_producto){   
